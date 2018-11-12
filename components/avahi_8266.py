fa4a35f600b06c2d89c3f1ecfd980baac20db6bf
gmorell <gmp@gmp.io>
2018-11-11 19:12:17 -0500

    initial commit and struct, basic zeroconf finder

---
+++ components/avahi_8266.py
@@ -0,0 +1,75 @@
+from autobahn import wamp
+from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+from zeroconf import ServiceBrowser, Zeroconf
+import os
+import socket
+import sys
+
+
+class ZeroConfSession(ApplicationSession):
+    current_items = {}
+
+    def __init__(self, config=None):
+        ApplicationSession.__init__(self, config)
+        print("component created")
+        self.zeroconf = Zeroconf()
+        self.listener = self
+        self.browser  = ServiceBrowser(self.zeroconf, "_ws2812._udp.local.", self.listener)
+        self.register(self)
+
+    def onConnect(self):
+        print("transport connected")
+        self.join(self.config.realm)
+
+    def onChallenge(self, challenge):
+        print("authentication challenge received")
+
+    def onJoin(self, details):
+        print("session joined")
+
+    def onLeave(self, details):
+        print("session left")
+
+    def onDisconnect(self):
+        print("transport disconnected")
+
+    @wamp.register("com.lambentri.edge.zeroconf.8266")
+    def get_list(self):
+        return {"devices":self.current_items}
+
+
+    def remove_service(self, zeroconf, type, name):
+        print("Service %s removed" % (name,))
+        del self.current_items[name]
+
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        print("Service %s added, service info: %s, %s" % (name, info, type))
+        print(socket.inet_ntoa(info.address))
+        self.current_items[name] = {"address":socket.inet_ntoa(info.address), "name":name, "port":info.port}
+
+
+# _ws2812._udp
+
+# class MyListener:
+#
+#     def remove_service(self, zeroconf, type, name):
+#         print("Service %s removed" % (name,))
+#
+#     def add_service(self, zeroconf, type, name):
+#         info = zeroconf.get_service_info(type, name)
+#         print("Service %s added, service info: %s" % (name, info))
+#
+# zeroconf = Zeroconf()
+# listener = MyListener()
+# browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+# try:
+#     input("Press enter to exit...\n\n")
+# finally:
+#     zeroconf.close()
+
+if __name__ == '__main__':
+    url = os.environ.get("XBAR_ROUTER", u"ws://127.0.0.1:8080/ws")
+    realm = u"realm1"
+    runner = ApplicationRunner(url, realm)
+    runner.run(ZeroConfSession)
